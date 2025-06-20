9d4b92b9fe5d3c22ffb6a6775fca8c68b670883a
Peter van Meijgaard <peter@vivid-web.com>
2025-06-20 16:54:04 +0200

    feat: initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,38 @@
+// src/routes/__root.tsx
+import type {ReactNode} from 'react'
+import {createRootRoute, HeadContent, Outlet, Scripts,} from '@tanstack/react-router'
+import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
+
+export const Route = createRootRoute({
+	head: () => ({
+		meta: [
+			{charSet: 'utf-8'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{title: 'TanStack Start Starter'},
+		],
+	}),
+	component: RootComponent,
+})
+
+function RootComponent() {
+	return (
+		<RootDocument>
+			<Outlet/>
+		</RootDocument>
+	)
+}
+
+function RootDocument({children}: Readonly<{ children: ReactNode }>) {
+	return (
+		<html>
+		<head>
+			<HeadContent/>
+		</head>
+		<body>
+		{children}
+		<TanStackRouterDevtools position="bottom-right"/>
+		<Scripts/>
+		</body>
+		</html>
+	)
+}
