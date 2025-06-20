a1ace2c354c398c5f72e9beddbd7d4c35e958fb9
Peter van Meijgaard <peter@vivid-web.com>
2025-06-20 18:54:15 +0200

    feat: add and configure TailwindCSS

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,3 @@
-// src/routes/__root.tsx
 import type { ReactNode } from "react";
 
 import { QueryClient } from "@tanstack/react-query";
@@ -11,10 +10,13 @@ import {
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+import appCss from "@/styles/app.css?url";
+
 export const Route = createRootRouteWithContext<{
 	queryClient: QueryClient;
 }>()({
 	head: () => ({
+		links: [{ rel: "stylesheet", href: appCss }],
 		meta: [
 			{ charSet: "utf-8" },
 			{ name: "viewport", content: "width=device-width, initial-scale=1" },
