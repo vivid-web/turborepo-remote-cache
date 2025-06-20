996ab7abb407545e7b531447b3cefdb62d148f3c
Peter van Meijgaard <peter@vivid-web.com>
2025-06-20 16:56:35 +0200

    feat: add and configure Prettier

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,37 +1,42 @@
-import * as React from 'react'
-import {createRootRoute, HeadContent, Outlet, Scripts,} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
+import * as React from "react";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
 
 export const Route = createRootRoute({
 	head: () => ({
 		meta: [
-			{charSet: 'utf-8'},
-			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
-			{title: 'TanStack Start Starter'},
+			{ charSet: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ title: "TanStack Start Starter" },
 		],
 	}),
 	component: RootComponent,
-})
+});
 
 function RootComponent() {
 	return (
 		<RootDocument>
-			<Outlet/>
+			<Outlet />
 		</RootDocument>
-	)
+	);
 }
 
-function RootDocument({children}: Readonly<{ children: React.ReactNode }>) {
+function RootDocument({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html>
-		<head>
-			<HeadContent/>
-		</head>
-		<body>
-		{children}
-		<TanStackRouterDevtools position="bottom-right"/>
-		<Scripts/>
-		</body>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<TanStackRouterDevtools position="bottom-right" />
+				<Scripts />
+			</body>
 		</html>
-	)
+	);
 }
