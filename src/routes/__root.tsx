8f53f24f682d8de1c130d186bb68e40fbb476a27
Peter van Meijgaard <peter@vivid-web.com>
2025-06-20 16:56:35 +0200

    feat: add and configure Prettier

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,43 +1,48 @@
 // src/routes/__root.tsx
-import type {ReactNode} from 'react'
-import {createRootRoute, HeadContent, Outlet, Scripts,} from '@tanstack/react-router'
+import type { ReactNode } from "react";
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
 
 export const Route = createRootRoute({
 	head: () => ({
 		meta: [
 			{
-				charSet: 'utf-8',
+				charSet: "utf-8",
 			},
 			{
-				name: 'viewport',
-				content: 'width=device-width, initial-scale=1',
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
 			},
 			{
-				title: 'TanStack Start Starter',
+				title: "TanStack Start Starter",
 			},
 		],
 	}),
 	component: RootComponent,
-})
+});
 
 function RootComponent() {
 	return (
 		<RootDocument>
-			<Outlet/>
+			<Outlet />
 		</RootDocument>
-	)
+	);
 }
 
-function RootDocument({children}: Readonly<{ children: ReactNode }>) {
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
 	return (
 		<html>
-		<head>
-			<HeadContent/>
-		</head>
-		<body>
-		{children}
-		<Scripts/>
-		</body>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+			</body>
 		</html>
-	)
+	);
 }
