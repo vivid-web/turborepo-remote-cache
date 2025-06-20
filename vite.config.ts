c4edad8fe19d130198e1926fe8d18381c351ae2d
Peter van Meijgaard <peter@vivid-web.com>
2025-06-20 16:56:35 +0200

    feat: add and configure Prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
-import {defineConfig} from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import {tanstackStart} from '@tanstack/react-start/plugin/vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
 	server: {
 		port: 3000,
 	},
-	plugins: [tsConfigPaths(), tanstackStart({customViteReactPlugin: true}), react()],
+	plugins: [
+		tsConfigPaths(),
+		tanstackStart({ customViteReactPlugin: true }),
+		react(),
+	],
 });
