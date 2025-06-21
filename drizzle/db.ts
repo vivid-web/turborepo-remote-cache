52c914a4c826fc0af415b69567c80fe4ce05c191
Peter van Meijgaard <peter@vivid-web.com>
2025-06-21 10:40:00 +0200

    feat: add and configure better auth

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,10 +1,13 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 
+import * as schema from "./schema";
+
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const client = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle({
 	casing: "snake_case",
 	client,
+	schema,
 });
