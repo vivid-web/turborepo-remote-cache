5328ed9e70eebcf41b7de2764f7532cce3be6a1e
Peter van Meijgaard <peter@vivid-web.com>
2025-06-21 10:40:00 +0200

    feat: add and configure better auth

--- drizzle/schema/index.ts
+++ drizzle/schema/index.ts
@@ -1,1 +1,66 @@
-// nothing to see here...
+import { createId as cuid } from "@paralleldrive/cuid2";
+import { boolean, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const user = pgTable("user", {
+	id: text()
+		.primaryKey()
+		.$defaultFn(() => cuid()),
+	name: text().notNull(),
+	email: text().notNull().unique(),
+	emailVerified: boolean()
+		.$defaultFn(() => false)
+		.notNull(),
+	image: text(),
+	createdAt: timestamp()
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+	updatedAt: timestamp()
+		.$defaultFn(() => /* @__PURE__ */ new Date())
+		.notNull(),
+});
+
+export const session = pgTable("session", {
+	id: text()
+		.primaryKey()
+		.$defaultFn(() => cuid()),
+	expiresAt: timestamp().notNull(),
+	token: text().notNull().unique(),
+	createdAt: timestamp().notNull(),
+	updatedAt: timestamp().notNull(),
+	ipAddress: text(),
+	userAgent: text(),
+	userId: text()
+		.notNull()
+		.references(() => user.id, { onDelete: "cascade" }),
+});
+
+export const account = pgTable("account", {
+	id: text()
+		.primaryKey()
+		.$defaultFn(() => cuid()),
+	accountId: text().notNull(),
+	providerId: text().notNull(),
+	userId: text()
+		.notNull()
+		.references(() => user.id, { onDelete: "cascade" }),
+	accessToken: text(),
+	refreshToken: text(),
+	idToken: text(),
+	accessTokenExpiresAt: timestamp(),
+	refreshTokenExpiresAt: timestamp(),
+	scope: text(),
+	password: text(),
+	createdAt: timestamp().notNull(),
+	updatedAt: timestamp().notNull(),
+});
+
+export const verification = pgTable("verification", {
+	id: text()
+		.primaryKey()
+		.$defaultFn(() => cuid()),
+	identifier: text().notNull(),
+	value: text().notNull(),
+	expiresAt: timestamp().notNull(),
+	createdAt: timestamp().$defaultFn(() => /* @__PURE__ */ new Date()),
+	updatedAt: timestamp().$defaultFn(() => /* @__PURE__ */ new Date()),
+});
