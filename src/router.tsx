0c3c650af655f0dad675c0ad1bdc8901b3216ce8
Peter van Meijgaard <peter@vivid-web.com>
2025-06-21 11:00:21 +0200

    feat: add and configure TanStack Query

--- src/router.tsx
+++ src/router.tsx
@@ -1,14 +1,19 @@
+import { QueryClient } from "@tanstack/react-query";
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
 
 import { routeTree } from "./routeTree.gen";
 
 export function createRouter() {
+	const queryClient = new QueryClient();
 	const router = createTanStackRouter({
+		context: { queryClient },
 		routeTree,
 		scrollRestoration: true,
+		defaultPreload: "intent",
 	});
 
-	return router;
+	return routerWithQueryClient(router, queryClient);
 }
 
 declare module "@tanstack/react-router" {
