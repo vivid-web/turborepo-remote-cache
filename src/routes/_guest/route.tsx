18463a943f63e0139c3be6328c9b8ef56985c2aa
Peter van Meijgaard <peter@vivid-web.com>
2025-07-12 10:38:55 +0200

    feat: add login form

--- src/routes/_guest/route.tsx
+++ src/routes/_guest/route.tsx
@@ -1,6 +1,7 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_guest")({
+	component: PathlessLayoutComponent,
 	beforeLoad: ({ context }) => {
 		if (!context.user) {
 			return;
@@ -9,3 +10,15 @@ export const Route = createFileRoute("/_guest")({
 		redirect({ throw: true, to: "/" });
 	},
 });
+
+function PathlessLayoutComponent() {
+	return (
+		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+			<div className="w-full max-w-sm md:max-w-3xl">
+				<div className="flex flex-col gap-6">
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	);
+}
