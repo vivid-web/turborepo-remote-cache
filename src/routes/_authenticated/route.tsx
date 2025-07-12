e9ead730b3606e6ca883da7813f91fc65a486321
Peter van Meijgaard <peter@vivid-web.com>
2025-07-12 16:49:31 +0200

    feat: add authenticated layout

--- src/routes/_authenticated/route.tsx
+++ src/routes/_authenticated/route.tsx
@@ -1,6 +1,32 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import {
+	createFileRoute,
+	invariant,
+	Link,
+	Outlet,
+	redirect,
+} from "@tanstack/react-router";
+import { DatabaseZapIcon } from "lucide-react";
+import * as React from "react";
+
+import { Separator } from "@/components/ui/separator";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarHeader,
+	SidebarInset,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
+
+import { RouteBreadcrumb } from "./-components/route-breadcrumb";
+import { UserDropdownMenu } from "./-components/user-dropdown-menu";
 
 export const Route = createFileRoute("/_authenticated")({
+	component: PathlessLayoutComponent,
 	beforeLoad: ({ context }) => {
 		if (context.user) {
 			return;
@@ -8,4 +34,72 @@ export const Route = createFileRoute("/_authenticated")({
 
 		redirect({ throw: true, to: "/login" });
 	},
+	loader: ({ context }) => {
+		invariant(context.user, "User should be authenticated by now");
+
+		return {
+			user: context.user,
+			crumb: "Home",
+		};
+	},
 });
+
+function PathlessLayoutComponent() {
+	const { user } = Route.useLoaderData();
+
+	return (
+		<SidebarProvider
+			style={
+				{
+					"--sidebar-width": "calc(var(--spacing) * 72)",
+					"--header-height": "calc(var(--spacing) * 12)",
+				} as React.CSSProperties
+			}
+		>
+			<Sidebar collapsible="offcanvas" variant="inset">
+				<SidebarHeader>
+					<SidebarMenu>
+						<SidebarMenuItem>
+							<SidebarMenuButton
+								asChild
+								className="data-[slot=sidebar-menu-button]:!p-1.5"
+							>
+								<Link to="/">
+									<DatabaseZapIcon className="!size-5" />
+									<span className="text-base font-semibold">
+										Turborepo Cache
+									</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					</SidebarMenu>
+				</SidebarHeader>
+
+				<SidebarContent>{/*todo: place content*/}</SidebarContent>
+
+				<SidebarFooter>
+					<SidebarMenu>
+						<SidebarMenuItem>
+							<UserDropdownMenu {...user} />
+						</SidebarMenuItem>
+					</SidebarMenu>
+				</SidebarFooter>
+			</Sidebar>
+			<SidebarInset>
+				<header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+					<div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+						<SidebarTrigger className="-ml-1" />
+						<Separator
+							orientation="vertical"
+							className="mx-2 data-[orientation=vertical]:h-4"
+						/>
+						<RouteBreadcrumb />
+					</div>
+				</header>
+				<div className="flex flex-col gap-4 p-4 md:gap-6 md:p-6">
+					<Outlet />
+				</div>
+			</SidebarInset>
+		</SidebarProvider>
+	);
+}
