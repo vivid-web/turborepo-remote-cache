296522738ced57a3ffa29981461f52e740626fe0
Peter van Meijgaard <peter@vivid-web.com>
2025-07-16 21:56:46 +0200

    feat: add edit user dialog component

--- src/routes/_authenticated.users.index/-server-fns.ts
+++ src/routes/_authenticated.users.index/-server-fns.ts
@@ -2,10 +2,43 @@ import { createServerFn } from "@tanstack/react-start";
 import { eq } from "drizzle-orm";
 import { db } from "drizzle/db";
 import { user } from "drizzle/schema";
+import { z } from "zod";
 
 import { auth } from "@/middlewares/auth";
 
-import { RemoveUserSchema, SearchSchema } from "./-schemas";
+import { EditUserSchema, RemoveUserSchema, SearchSchema } from "./-schemas";
+
+const checkIfEmailUnique = createServerFn({ method: "POST" })
+	.middleware([auth])
+	.validator(
+		z.object({
+			email: z.email(),
+			id: z.string().min(1).optional(),
+		}),
+	)
+	.handler(async ({ data: { email, id } }) => {
+		const foundUser = await db.query.user.findFirst({
+			columns: { id: true },
+			where: (users, { eq, and, not }) => {
+				const emailQuery = eq(users.email, email);
+
+				if (!id) {
+					return emailQuery;
+				}
+
+				return and(emailQuery, not(eq(users.id, id)));
+			},
+		});
+
+		return !foundUser;
+	});
+
+const editUser = createServerFn({ method: "POST" })
+	.middleware([auth])
+	.validator(EditUserSchema)
+	.handler(async ({ data: { id, ...data } }) => {
+		await db.update(user).set(data).where(eq(user.id, id));
+	});
 
 const getAllUsers = createServerFn({ method: "GET" })
 	.middleware([auth])
@@ -43,4 +76,4 @@ const removeUser = createServerFn({ method: "POST" })
 		await db.delete(user).where(eq(user.id, data.id));
 	});
 
-export { getAllUsers, getTotalUsers, removeUser };
+export { checkIfEmailUnique, editUser, getAllUsers, getTotalUsers, removeUser };
