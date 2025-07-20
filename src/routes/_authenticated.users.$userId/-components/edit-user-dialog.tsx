01facb9e7e4b88173b8ea990500e59bd312c08ff
Peter van Meijgaard <peter@vivid-web.com>
2025-07-20 11:51:33 +0200

    feat: add edit user dialog component

---
+++ src/routes/_authenticated.users.$userId/-components/edit-user-dialog.tsx
@@ -0,0 +1,176 @@
+import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
+import { getRouteApi, useRouter } from "@tanstack/react-router";
+import { Loader2Icon } from "lucide-react";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "@/components/ui/dialog";
+import { useAppForm } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { singleUserQueryOptions } from "../-queries";
+import { EditUserSchema } from "../-schemas";
+import { checkIfEmailUnique, editUser } from "../-server-fns";
+
+const EDIT_USER_FORM_ID = "edit-user-form";
+
+const route = getRouteApi("/_authenticated/users/$userId");
+
+function EditUserDialog({ children }: React.PropsWithChildren) {
+	const router = useRouter();
+	const params = route.useParams();
+	const queryClient = useQueryClient();
+
+	const { data: user } = useSuspenseQuery(singleUserQueryOptions(params));
+
+	const [isOpen, setIsOpen] = React.useState(false);
+
+	const form = useAppForm({
+		defaultValues: {
+			id: user.id,
+			name: user.name,
+			email: user.email,
+		},
+		validators: {
+			onChange: EditUserSchema,
+			onSubmitAsync: async ({ value }) => {
+				if (await checkIfEmailUnique({ data: value })) {
+					return null;
+				}
+
+				return {
+					fields: {
+						email: {
+							message:
+								"Email already exists. Please use a different email address.",
+						},
+					},
+				};
+			},
+		},
+		onSubmit: async ({ value: data, formApi }) => {
+			await editUser({ data });
+
+			await queryClient.invalidateQueries();
+			await router.invalidate();
+
+			setIsOpen(false);
+
+			formApi.reset();
+		},
+	});
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		void form.handleSubmit();
+	};
+
+	return (
+		<Dialog onOpenChange={setIsOpen} open={isOpen}>
+			<DialogTrigger asChild>{children}</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>Edit User</DialogTitle>
+					<DialogDescription>Update user information</DialogDescription>
+				</DialogHeader>
+				<form.AppForm>
+					<form
+						noValidate
+						onSubmit={handleSubmit}
+						className="grid gap-4"
+						id={EDIT_USER_FORM_ID}
+					>
+						<form.AppField
+							name="id"
+							children={(field) => (
+								<input
+									type="hidden"
+									name={field.name}
+									value={field.state.value}
+								/>
+							)}
+						/>
+
+						<form.AppField
+							name="name"
+							children={(field) => (
+								<field.FormItem>
+									<field.FormLabel>Name</field.FormLabel>
+									<field.FormControl>
+										<Input
+											placeholder="John Doe"
+											name={field.name}
+											value={field.state.value}
+											onChange={(e) => {
+												field.handleChange(e.target.value);
+											}}
+											onBlur={field.handleBlur}
+											type="text"
+										/>
+									</field.FormControl>
+									<field.FormMessage className="text-xs" />
+								</field.FormItem>
+							)}
+						/>
+
+						<form.AppField
+							name="email"
+							children={(field) => (
+								<field.FormItem>
+									<field.FormLabel>Email</field.FormLabel>
+									<field.FormControl>
+										<Input
+											placeholder="john@doe.com"
+											name={field.name}
+											value={field.state.value}
+											onChange={(e) => {
+												field.handleChange(e.target.value);
+											}}
+											onBlur={field.handleBlur}
+											type="email"
+										/>
+									</field.FormControl>
+									<field.FormMessage className="text-xs" />
+								</field.FormItem>
+							)}
+						/>
+					</form>
+				</form.AppForm>
+				<DialogFooter>
+					<DialogClose asChild>
+						<Button variant="outline">Cancel</Button>
+					</DialogClose>
+					<form.Subscribe
+						selector={(state) => [state.canSubmit, state.isSubmitting]}
+						children={([canSubmit, isSubmitting]) => (
+							<Button
+								type="submit"
+								form={EDIT_USER_FORM_ID}
+								disabled={!canSubmit}
+							>
+								{isSubmitting ? (
+									<Loader2Icon className="animate-spin" />
+								) : (
+									"Update user"
+								)}
+							</Button>
+						)}
+					/>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+export { EditUserDialog };
--- src/routes/_authenticated.users.$userId/-schemas.ts
+++ src/routes/_authenticated.users.$userId/-schemas.ts
@@ -1,11 +1,22 @@
 import { z } from "zod";
 
+const EditUserSchema = z.object({
+	id: z.string(),
+	name: z
+		.string()
+		.min(1, "Name is required")
+		.max(1024, "Name must be less than 1024 characters"),
+	email: z.email(),
+});
+
 const ParamsSchema = z.object({
 	userId: z.string().min(1),
 });
 
+type EditUserInput = z.input<typeof EditUserSchema>;
+
 type ParamsInput = z.input<typeof ParamsSchema>;
 
-export { ParamsSchema };
+export { EditUserSchema, ParamsSchema };
 
-export type { ParamsInput };
+export type { EditUserInput, ParamsInput };
