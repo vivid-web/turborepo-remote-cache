b4573f788eccf5e36b42c760c79e00dc1e66ba94
Peter van Meijgaard <peter@vivid-web.com>
2025-07-21 21:11:04 +0200

    feat: add search teams form component

---
+++ src/routes/_authenticated.teams.index/-components/search-teams-form.tsx
@@ -0,0 +1,68 @@
+import { getRouteApi } from "@tanstack/react-router";
+import { SearchIcon } from "lucide-react";
+import * as React from "react";
+import { z } from "zod";
+
+import { useAppForm } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+import { QuerySchema } from "../-schemas";
+
+const route = getRouteApi("/_authenticated/teams/");
+
+function SearchTeamsForm() {
+	const search = route.useSearch();
+	const navigate = route.useNavigate();
+
+	const form = useAppForm({
+		defaultValues: { query: search.query ?? "" },
+		validators: {
+			onChange: z.object({
+				query: QuerySchema,
+			}),
+		},
+		onSubmit: async ({ value }) => {
+			await navigate({
+				search: (curr) => ({ ...curr, query: value.query || undefined }),
+			});
+		},
+	});
+
+	const handleSubmit = (event: React.FormEvent) => {
+		event.preventDefault();
+		event.stopPropagation();
+
+		void form.handleSubmit();
+	};
+
+	return (
+		<form.AppForm>
+			<form onSubmit={handleSubmit}>
+				<form.AppField
+					name="query"
+					children={(field) => (
+						<field.FormItem className="relative w-64">
+							<field.FormLabel className="absolute top-1/2 left-3 -translate-y-1/2 transform text-muted-foreground">
+								<SearchIcon className="h-4 w-4" />
+							</field.FormLabel>
+							<field.FormControl>
+								<Input
+									placeholder="Search teams..."
+									className="pl-10"
+									name={field.name}
+									value={field.state.value}
+									onChange={(e) => {
+										field.handleChange(e.target.value);
+									}}
+									onBlur={field.handleBlur}
+								/>
+							</field.FormControl>
+						</field.FormItem>
+					)}
+				/>
+			</form>
+		</form.AppForm>
+	);
+}
+
+export { SearchTeamsForm };
