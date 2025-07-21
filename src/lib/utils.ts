3be51d3449da6ba2b848908d8cdf71b76458838b
Peter van Meijgaard <peter@vivid-web.com>
2025-07-21 20:40:00 +0200

    feat: add slugify utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: Array<ClassValue>) {
 	return twMerge(clsx(inputs));
 }
+
+export function slugify(...args: Array<string>) {
+	const value = args.join(" ");
+
+	return value
+		.normalize("NFD") // split an accented letter in the base letter and the acent
+		.replace(/[\u0300-\u036f]/g, "") // remove all previously split accents
+		.toLowerCase()
+		.trim()
+		.replace(/[^a-z0-9 ]/g, "") // remove all chars not letters, numbers and spaces (to be replaced)
+		.replace(/\s+/g, "-"); // separator
+}
