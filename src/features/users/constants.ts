d51e613126ea969ab51486058891489b77127250
Peter van Meijgaard <peter@vivid-web.com>
2025-07-27 17:15:41 +0200

    chore: move user constants to users feature

--- src/features/users/constants.ts
+++ src/features/users/constants.ts
@@ -1,3 +1,3 @@
-export const ADD_NEW_USER_FORM_ID = "add-new-user-form";
 export const EDIT_USER_FORM_ID = "edit-user-form";
+export const ADD_NEW_USER_FORM_ID = "add-new-user-form";
 export const REMOVE_USER_FORM_ID = "remove-user-form";
