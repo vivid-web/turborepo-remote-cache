9d2167125b736546b1a49c3df36aa727276ff350
Peter van Meijgaard <peter@vivid-web.com>
2025-07-29 14:36:03 +0200

    chore: refactor user info card

---
+++ src/features/users/server-fns/get-user-general-info.ts
@@ -0,0 +1,43 @@
+import { notFound } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { desc, eq } from "drizzle-orm";
+import { db } from "drizzle/db";
+import { session, user } from "drizzle/schema";
+import { z } from "zod";
+
+import { IdSchema } from "@/lib/schemas";
+import { auth } from "@/middlewares/auth";
+
+const getUserGeneralInfo = createServerFn({ method: "GET" })
+	.middleware([auth])
+	.validator(z.object({ userId: IdSchema }))
+	.handler(async ({ data: { userId } }) => {
+		const [foundUser] = await db
+			.select({
+				email: user.email,
+				name: user.name,
+				image: user.image,
+				createdAt: user.createdAt,
+			})
+			.from(user)
+			.where(eq(user.id, userId))
+			.limit(1);
+
+		const [foundSession] = await db
+			.select({ lastLoggedInAt: session.createdAt })
+			.from(session)
+			.where(eq(session.userId, userId))
+			.orderBy(desc(session.createdAt))
+			.limit(1);
+
+		if (!foundUser) {
+			throw notFound();
+		}
+
+		return {
+			...foundUser,
+			...foundSession,
+		};
+	});
+
+export { getUserGeneralInfo };
