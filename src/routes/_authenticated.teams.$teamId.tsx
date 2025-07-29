10b583fcc07d6a1b233ab67153ad0f1371c34d3d
Peter van Meijgaard <peter@vivid-web.com>
2025-07-29 17:34:03 +0200

    feat: add all users for team card

--- src/routes/_authenticated.teams.$teamId.tsx
+++ src/routes/_authenticated.teams.$teamId.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
 
 import { getBreadcrumbForTeam } from "@/features/teams/server-fns/get-breadcrumb-for-team";
+import { AllUsersForTeamCard } from "@/features/users/components/all-users-for-team-card";
 import { TotalUsersForTeamCard } from "@/features/users/components/total-users-for-team-card";
 import { IdSchema } from "@/lib/schemas";
 
@@ -13,9 +14,10 @@ export const Route = createFileRoute("/_authenticated/teams/$teamId")({
 	loader: async ({ context: { queryClient }, params }) => {
 		const crumb = await getBreadcrumbForTeam({ data: params });
 
-		await queryClient.ensureQueryData(
-			TotalUsersForTeamCard.queryOptions(params),
-		);
+		await Promise.all([
+			queryClient.ensureQueryData(TotalUsersForTeamCard.queryOptions(params)),
+			queryClient.ensureQueryData(AllUsersForTeamCard.queryOptions(params)),
+		]);
 
 		return { crumb };
 	},
@@ -38,6 +40,8 @@ function RouteComponent() {
 			<div className="grid gap-4 md:grid-cols-3">
 				<TotalUsersForTeamCard teamId={teamId} />
 			</div>
+
+			<AllUsersForTeamCard teamId={teamId} />
 		</div>
 	);
 }
