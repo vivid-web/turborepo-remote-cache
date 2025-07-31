2e5239adc57bef25f949e85c5c08109e00bc9273
Peter van Meijgaard <peter@vivid-web.com>
2025-07-31 21:38:15 +0200

    feat: add attach users to team form id constant

--- src/features/users/constants.ts
+++ src/features/users/constants.ts
@@ -1,3 +1,4 @@
+export const ATTACH_USERS_TO_TEAM_FORM_ID = "attach-users-to-team-form";
 export const EDIT_USER_FORM_ID = "edit-user-form";
 export const ADD_NEW_USER_FORM_ID = "add-new-user-form";
 export const REMOVE_USER_FORM_ID = "remove-user-form";
