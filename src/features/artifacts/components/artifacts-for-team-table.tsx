da48283f99e032f42d99bff1cd1347dbf954a9c9
Peter van Meijgaard <peter@vivid-web.com>
2025-08-16 16:57:15 +0200

    feat: add artifacts for team table component

---
+++ src/features/artifacts/components/artifacts-for-team-table.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
+
+import { formatCreatedDate } from "../utils";
+
+type Artifact = {
+	artifactId: string;
+	createdAt: Date;
+	hash: string;
+};
+
+function FilledRow({ hash, createdAt }: Artifact) {
+	return (
+		<TableRow>
+			<TableCell className="max-w-[200px] truncate font-mono text-sm">
+				{hash}
+			</TableCell>
+			<TableCell className="text-muted-foreground">
+				{formatCreatedDate(createdAt)}
+			</TableCell>
+		</TableRow>
+	);
+}
+
+function EmptyRow() {
+	return (
+		<TableRow>
+			<TableCell colSpan={2}>No artifacts found...</TableCell>
+		</TableRow>
+	);
+}
+
+function Layout({ children }: React.PropsWithChildren) {
+	return (
+		<Table>
+			<TableHeader>
+				<TableRow>
+					<TableHead>Hash</TableHead>
+					<TableHead>Created</TableHead>
+				</TableRow>
+			</TableHeader>
+			<TableBody>{children}</TableBody>
+		</Table>
+	);
+}
+
+function ArtifactsForTeamTable({ artifacts }: { artifacts: Array<Artifact> }) {
+	if (artifacts.length === 0) {
+		return (
+			<Layout>
+				<EmptyRow />
+			</Layout>
+		);
+	}
+
+	return (
+		<Layout>
+			{artifacts.map((artifact) => (
+				<FilledRow {...artifact} key={artifact.artifactId} />
+			))}
+		</Layout>
+	);
+}
+
+export { ArtifactsForTeamTable };
