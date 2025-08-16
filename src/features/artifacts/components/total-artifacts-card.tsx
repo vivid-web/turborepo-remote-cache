f83e501fae6c387391997b783adbee417d6352aa
Peter van Meijgaard <peter@vivid-web.com>
2025-08-16 14:51:58 +0200

    feat: add total artifacts card component

---
+++ src/features/artifacts/components/total-artifacts-card.tsx
@@ -0,0 +1,46 @@
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import { createServerFn } from "@tanstack/react-start";
+import { db } from "drizzle/db";
+import { artifact } from "drizzle/schema";
+import { PackageIcon } from "lucide-react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@/middlewares/auth";
+
+import { ARTIFACTS_QUERY_KEY } from "../constants";
+
+const getTotalArtifacts = createServerFn({ method: "GET" })
+	.middleware([auth])
+	.handler(async () => db.$count(artifact));
+
+function totalArtifactsQueryOptions() {
+	return queryOptions({
+		queryFn: async () => getTotalArtifacts(),
+		queryKey: [ARTIFACTS_QUERY_KEY, "total-artifacts"],
+	});
+}
+
+function TotalArtifactsCard() {
+	const query = useSuspenseQuery(totalArtifactsQueryOptions());
+
+	return (
+		<Card>
+			<CardHeader className="flex flex-row items-center justify-between">
+				<div className="flex items-center gap-2">
+					<PackageIcon className="!h-4 !w-4" />
+					<CardTitle className="font-medium">Total Artifacts</CardTitle>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">{query.data}</div>
+				<p className="text-xs text-muted-foreground">
+					Artifacts uploaded in the system
+				</p>
+			</CardContent>
+		</Card>
+	);
+}
+
+TotalArtifactsCard.queryOptions = totalArtifactsQueryOptions;
+
+export { TotalArtifactsCard };
