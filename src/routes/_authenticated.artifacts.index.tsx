2da2b1b86ebdf1e3f0deaf0f9d40e33f3576330f
Peter van Meijgaard <peter@vivid-web.com>
2025-08-16 14:54:31 +0200

    feat: add all artifacts card component

--- src/routes/_authenticated.artifacts.index.tsx
+++ src/routes/_authenticated.artifacts.index.tsx
@@ -1,17 +1,34 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
+import { AllArtifactsCard } from "@/features/artifacts/components/all-artifacts-card";
 import { TotalArtifactsCard } from "@/features/artifacts/components/total-artifacts-card";
+import { QuerySchema } from "@/features/artifacts/schemas";
 
 export const Route = createFileRoute("/_authenticated/artifacts/")({
 	component: RouteComponent,
-	loader: async ({ context: { queryClient } }) => {
+	validateSearch: z.object({
+		query: QuerySchema.optional(),
+	}),
+	loaderDeps: ({ search }) => search,
+	loader: async ({ context: { queryClient }, deps: search }) => {
 		await Promise.all([
+			queryClient.ensureQueryData(AllArtifactsCard.queryOptions(search)),
 			queryClient.ensureQueryData(TotalArtifactsCard.queryOptions()),
 		]);
 	},
 });
 
 function RouteComponent() {
+	const navigate = Route.useNavigate();
+	const search = Route.useSearch();
+
+	const handleSearch = async (query?: string) => {
+		await navigate({
+			search: (curr) => ({ ...curr, query }),
+		});
+	};
+
 	return (
 		<div className="grid gap-6">
 			<div>
@@ -24,6 +41,8 @@ function RouteComponent() {
 			<div className="grid gap-4 md:grid-cols-3">
 				<TotalArtifactsCard />
 			</div>
+
+			<AllArtifactsCard query={search.query} onSearch={handleSearch} />
 		</div>
 	);
 }
