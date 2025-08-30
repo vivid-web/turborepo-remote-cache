52be0b2abb2485814ef65a147d81e70b7077a3d4
Peter van Meijgaard <peter@vivid-web.com>
2025-08-30 11:48:48 +0200

    feat: migrate to monorepo

---
+++ apps/admin/src/features/artifacts/components/total-artifacts-for-team-card.tsx
@@ -0,0 +1,66 @@
+import { queryOptions, useSuspenseQuery } from "@tanstack/react-query";
+import { notFound } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { count, eq } from "drizzle-orm";
+import { db } from "drizzle/db";
+import { artifact } from "drizzle/schema";
+import { PackageIcon } from "lucide-react";
+import { z } from "zod";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { IdSchema } from "@/lib/schemas";
+import { auth } from "@/middlewares/auth";
+
+import { ARTIFACTS_QUERY_KEY } from "../constants";
+
+type Params = z.input<typeof ParamsSchema>;
+
+const ParamsSchema = z.object({
+	teamId: IdSchema,
+});
+
+const getTotalArtifactsForTeam = createServerFn({ method: "GET" })
+	.middleware([auth])
+	.validator(ParamsSchema)
+	.handler(async ({ data: { teamId } }) => {
+		const [item] = await db
+			.select({ count: count() })
+			.from(artifact)
+			.where(eq(artifact.teamId, teamId));
+
+		if (!item) {
+			throw notFound();
+		}
+
+		return item.count;
+	});
+
+function totalArtifactsForTeamQueryOptions(params: Params) {
+	return queryOptions({
+		queryFn: async () => getTotalArtifactsForTeam({ data: params }),
+		queryKey: [ARTIFACTS_QUERY_KEY, "total-artifacts-for-team", params.teamId],
+	});
+}
+
+function TotalArtifactsForTeamCard({ teamId }: Params) {
+	const query = useSuspenseQuery(totalArtifactsForTeamQueryOptions({ teamId }));
+
+	return (
+		<Card>
+			<CardHeader className="flex flex-row items-center justify-between">
+				<div className="flex items-center gap-2">
+					<PackageIcon className="!h-4 !w-4" />
+					<CardTitle className="font-medium">Total Artifacts</CardTitle>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">{query.data}</div>
+				<p className="text-muted-foreground text-xs">Artifacts for this team</p>
+			</CardContent>
+		</Card>
+	);
+}
+
+TotalArtifactsForTeamCard.queryOptions = totalArtifactsForTeamQueryOptions;
+
+export { TotalArtifactsForTeamCard };
