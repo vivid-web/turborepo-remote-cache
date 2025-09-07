e41fba23dab14dae5b3034d9ff959e9d4736a7d4
Peter van Meijgaard <peter@vivid-web.com>
2025-09-07 14:21:11 +0200

    feat: add reroll API key for account dialog

--- apps/admin/src/features/api-keys/constants.ts
+++ apps/admin/src/features/api-keys/constants.ts
@@ -1,5 +1,6 @@
 export const ADD_NEW_API_KEY_FOR_ACCOUNT_FORM_ID =
 	"add-new-api-key-for-account-form";
+export const REROLL_API_KEY_FORM_ID = "reroll-api-key-form";
 
 export const API_KEYS_QUERY_KEY = "api-keys";
 
