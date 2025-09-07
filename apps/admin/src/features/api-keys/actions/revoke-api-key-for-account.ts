68d4c038822e1a0d3d8ca82631dcc416ee12e342
Peter van Meijgaard <peter@vivid-web.com>
2025-09-07 14:27:55 +0200

    feat: add revoke API key for account alert dialog

---
+++ apps/admin/src/features/api-keys/actions/revoke-api-key-for-account.ts
@@ -0,0 +1,25 @@
+import { createServerFn } from "@tanstack/react-start";
+import { and, eq, SQL } from "@turborepo-remote-cache/db";
+import { db } from "@turborepo-remote-cache/db/client";
+import { apiKey } from "@turborepo-remote-cache/db/schema";
+import { z } from "zod";
+
+import { IdSchema } from "@/lib/schemas";
+import { auth } from "@/middlewares/auth";
+
+const revokeApiKeyForAccount = createServerFn({ method: "POST" })
+	.middleware([auth])
+	.validator(z.object({ apiKeyId: IdSchema }))
+	.handler(async ({ data, context: { user } }) => {
+		const filters: Array<SQL> = [];
+
+		filters.push(eq(apiKey.userId, user.id));
+		filters.push(eq(apiKey.id, data.apiKeyId));
+
+		await db
+			.update(apiKey)
+			.set({ revokedAt: new Date() })
+			.where(and(...filters));
+	});
+
+export { revokeApiKeyForAccount };
