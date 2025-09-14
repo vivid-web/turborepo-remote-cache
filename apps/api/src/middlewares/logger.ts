e710d099075b3e2c6845255267ac30f09fc77186
Peter van Meijgaard <peter@vivid-web.com>
2025-09-14 15:02:01 +0200

    chore: rename log middleware to logger

--- apps/api/src/middlewares/logger.ts
+++ apps/api/src/middlewares/logger.ts
@@ -1,11 +1,11 @@
-import { pinoLogger as logger } from "hono-pino";
+import { pinoLogger } from "hono-pino";
 import pino from "pino";
 import pretty from "pino-pretty";
 
 import { env } from "../env.js";
 
-export function logMiddleware() {
-	return logger({
+export function logger() {
+	return pinoLogger({
 		pino: pino(
 			{
 				level: env.LOG_LEVEL || "info",
