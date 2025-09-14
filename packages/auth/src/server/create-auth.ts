f91a0914e578a6b360cc39208a5f19710260cf8e
Peter van Meijgaard <peter@vivid-web.com>
2025-09-14 16:31:33 +0200

    chore: add missing environment variables

--- packages/auth/src/server/create-auth.ts
+++ packages/auth/src/server/create-auth.ts
@@ -2,8 +2,12 @@ import { db } from "@turborepo-remote-cache/db/client";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
+import { env } from "./env.js";
+
 export function createAuth() {
 	return betterAuth({
+		baseURL: env.BETTER_AUTH_URL,
+		secret: env.BETTER_AUTH_SECRET,
 		emailAndPassword: {
 			enabled: true,
 		},
