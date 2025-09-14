af84faefa6f629ee320db1152d75c7d6e739fd2d
Peter van Meijgaard <peter@vivid-web.com>
2025-09-14 14:59:55 +0200

    feat: add log middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,14 +1,14 @@
 import { Hono } from "hono";
-import { logger } from "hono/logger";
 import { requestId } from "hono/request-id";
 import { notFound, onError } from "stoker/middlewares";
 
+import { logMiddleware } from "./middlewares/log-middleware.js";
 import artifacts from "./routes/artifacts.js";
 
 const app = new Hono();
 
 app.use(requestId());
-app.use(logger());
+app.use(logMiddleware());
 app.notFound(notFound);
 app.onError(onError);
 
