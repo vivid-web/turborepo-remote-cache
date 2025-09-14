0a14081e2e511d075628e209b767310fa2b352e8
Peter van Meijgaard <peter@vivid-web.com>
2025-09-14 16:06:37 +0200

    feat: move authentication to API application

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -7,6 +7,14 @@ import tsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
 	server: {
 		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:3001",
+				rewrite: (path) => path.replace(/^\/api/, ""),
+				changeOrigin: true,
+				secure: false,
+			},
+		},
 	},
 	plugins: [
 		tsConfigPaths(),
