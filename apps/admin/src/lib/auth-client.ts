0b848e5a64b01b9e23146f43ec276a61216c06aa
Peter van Meijgaard <peter@vivid-web.com>
2025-09-18 23:03:42 +0200

    chore: remove host from auth client

--- apps/admin/src/lib/auth-client.ts
+++ apps/admin/src/lib/auth-client.ts
@@ -1,5 +1,3 @@
 import { createAuthClient } from "better-auth/react";
 
-export const { signIn, signUp, useSession, signOut } = createAuthClient({
-	baseURL: "http://localhost:3000",
-});
+export const { signIn, signUp, useSession, signOut } = createAuthClient();
