0eee8d37e67bff3d1cbd3ac721db08e0c9cd0833
Peter van Meijgaard <peter@vivid-web.com>
2025-09-19 08:25:19 +0200

    chore: add options to create auth instead of env variables

--- packages/auth/src/create-auth.ts
+++ packages/auth/src/create-auth.ts
@@ -1,15 +1,13 @@
 import { db } from "@turborepo-remote-cache/db/client";
-import { betterAuth } from "better-auth";
+import { betterAuth, type BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
-import { env } from "./env.js";
+type Options = Pick<BetterAuthOptions, "baseURL" | "secret" | "trustedOrigins">;
 
-export function createAuth() {
+export function createAuth(options?: Options) {
 	return betterAuth({
-		baseURL: env.BETTER_AUTH_URL,
+		...options,
 		basePath: "/auth",
-		secret: env.BETTER_AUTH_SECRET,
-		trustedOrigins: ["http://localhost:3000", "http://localhost:3001"],
 		emailAndPassword: {
 			enabled: true,
 		},
