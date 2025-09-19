d695c3d8e12d093f88146492e6098be5926f1c19
Peter van Meijgaard <peter@vivid-web.com>
2025-09-19 08:39:16 +0200

    chore: make Node server optional and simplify API app

---
+++ apps/api/scripts/serve.js
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import { serve } from "@hono/node-server";
+
+import { app } from "../dist/index.js";
+
+const server = serve({
+	fetch: app.fetch,
+	port: 3001,
+});
+
+process.on("SIGINT", () => {
+	server.close();
+
+	process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+	server.close((err) => {
+		if (err) {
+			console.error(err);
+
+			process.exit(1);
+		}
+
+		process.exit(0);
+	});
+});
