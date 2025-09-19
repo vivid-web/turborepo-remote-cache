f770dd361d83be6375c46e7a64b8fbd1daa5a5f3
Peter van Meijgaard <peter@vivid-web.com>
2025-09-19 09:20:08 +0200

    feat: add Netlify hosting to admin application

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -4,13 +4,21 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
+function getTarget() {
+	if (process.env.NETLIFY === "true") {
+		return "netlify";
+	}
+
+	return "node-server";
+}
+
 export default defineConfig({
 	server: {
 		port: 3000,
 	},
 	plugins: [
 		tsConfigPaths(),
-		tanstackStart({ customViteReactPlugin: true }),
+		tanstackStart({ customViteReactPlugin: true, target: getTarget() }),
 		react(),
 		tailwindcss(),
 	],
