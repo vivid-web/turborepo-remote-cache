c49b8f794509acc71dd0655ae78fb63735bb3959
Peter van Meijgaard <peter@vivid-web.com>
2025-09-19 18:03:54 +0200

    chore: use storage package in API application

--- apps/api/src/storage/index.ts
+++
@@ -1,2 +0,0 @@
-export { createLocalStorage } from "./create-local-storage.js";
-export { createNetlifyBlobsStorage } from "./create-netlify-blobs-storage.js";
--- apps/api/src/storage/types.ts
+++
@@ -1,6 +0,0 @@
-export type Storage = {
-	delete: (hash: string) => Promise<void>;
-	get: (hash: string) => Promise<Blob>;
-	has: (hash: string) => Promise<boolean>;
-	set: (hash: string, blob: Blob) => Promise<void>;
-};
