4d319acf4baa80b8ffe0f6c266db3fa9fcc281d9
Peter van Meijgaard <peter@vivid-web.com>
2025-09-19 09:20:08 +0200

    feat: add Netlify hosting to admin application

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -1,10 +1,15 @@
 import { createEnv } from "@t3-oss/env-core";
+import { netlify } from "@t3-oss/env-core/presets-zod";
 import { process } from "std-env";
 import { z } from "zod";
 
 export const env = createEnv({
+	extends: [netlify()],
 	server: {
 		API_URL: z.url(),
+
+		// Netlify override as the `netlify` preset doesn't cast NETLIFY to a boolean
+		NETLIFY: z.stringbool().optional(),
 	},
 	runtimeEnv: process.env,
 	emptyStringAsUndefined: true,
