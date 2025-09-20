8a1fbf6a2ed5865967b62e42122750941144a1d3
Peter van Meijgaard <peter@vivid-web.com>
2025-09-20 15:41:10 +0200

    chore: use logger middleware

--- apps/admin/src/start.ts
+++ apps/admin/src/start.ts
@@ -1,10 +1,13 @@
 import * as Sentry from "@sentry/tanstackstart-react";
 import { createMiddleware, createStart } from "@tanstack/react-start";
 
+import { logger } from "./middlewares/logger";
+
 const sentry = createMiddleware({ type: "function" }).server(
 	Sentry.sentryGlobalServerMiddlewareHandler(),
 );
 
 export const startInstance = createStart(() => ({
+	requestMiddleware: [logger],
 	functionMiddleware: [sentry],
 }));
