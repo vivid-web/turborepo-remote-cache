5abc38a06f59cda5a9f49a18cb5af201e304da69
Peter van Meijgaard <peter@vivid-web.com>
2025-09-27 15:06:45 +0200

    chore: add React Start cookies plugin

--- packages/auth/src/create-auth.ts
+++ packages/auth/src/create-auth.ts
@@ -1,12 +1,14 @@
 import { db } from "@turborepo-remote-cache/db/client";
 import { betterAuth, type BetterAuthOptions } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { reactStartCookies } from "better-auth/react-start";
 
 type Options = Pick<BetterAuthOptions, "baseURL" | "secret">;
 
 export function createAuth(options?: Options) {
 	return betterAuth({
 		...options,
+		plugins: [reactStartCookies()],
 		emailAndPassword: {
 			enabled: true,
 		},
