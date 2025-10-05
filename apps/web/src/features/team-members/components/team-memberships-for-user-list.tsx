ce57c2733d9254fb0460a4b095cd723c809586ab
Peter van Meijgaard <peter@vivid-web.com>
2025-10-05 13:39:22 +0200

    feat: add no team memberships placeholder component

--- apps/web/src/features/team-members/components/team-memberships-for-user-list.tsx
+++ apps/web/src/features/team-members/components/team-memberships-for-user-list.tsx
@@ -1,5 +1,5 @@
 import { Link } from "@tanstack/react-router";
-import { MoreVerticalIcon, SquarePlusIcon, UsersIcon } from "lucide-react";
+import { MoreVerticalIcon, UsersIcon } from "lucide-react";
 import * as React from "react";
 import { lazily } from "react-lazily";
 
@@ -11,14 +11,6 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
-	Empty,
-	EmptyContent,
-	EmptyDescription,
-	EmptyHeader,
-	EmptyMedia,
-	EmptyTitle,
-} from "@/components/ui/empty";
-import {
 	Item,
 	ItemActions,
 	ItemContent,
@@ -28,9 +20,7 @@ import {
 } from "@/components/ui/item";
 import { Spinner } from "@/components/ui/spinner";
 
-const { AttachTeamMembersToUserDialog } = lazily(
-	() => import("./attach-team-members-to-user-dialog"),
-);
+import { NoTeamMembershipsPlaceholder } from "./no-team-memberships-placeholder";
 
 const { DetachTeamMemberFromUserAlertDialog } = lazily(
 	() => import("./detach-team-member-from-user-alert-dialog"),
@@ -99,44 +89,12 @@ function FilledListItem({ name, teamId, userId }: Params & Team) {
 	);
 }
 
-function EmptyListItem({ userId }: Params) {
-	return (
-		<Empty className="border border-dashed">
-			<EmptyHeader>
-				<EmptyMedia variant="icon">
-					<UsersIcon />
-				</EmptyMedia>
-				<EmptyTitle>No Teams Found</EmptyTitle>
-				<EmptyDescription>
-					This user is not a member of any teams
-				</EmptyDescription>
-			</EmptyHeader>
-			<EmptyContent>
-				<React.Suspense
-					fallback={
-						<Button className="gap-2" disabled>
-							<Spinner />
-						</Button>
-					}
-				>
-					<AttachTeamMembersToUserDialog userId={userId}>
-						<Button className="gap-2">
-							<SquarePlusIcon className="!h-4 !w-4" />
-							Attach Teams
-						</Button>
-					</AttachTeamMembersToUserDialog>
-				</React.Suspense>
-			</EmptyContent>
-		</Empty>
-	);
-}
-
 function TeamMembershipsForUserList({
 	teams,
 	userId,
 }: Params & { teams: Array<Team> }) {
 	if (teams.length === 0) {
-		return <EmptyListItem userId={userId} />;
+		return <NoTeamMembershipsPlaceholder userId={userId} />;
 	}
 
 	return (
