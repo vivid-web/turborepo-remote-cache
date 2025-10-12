a007ad188d383ad1b5c68208b8ca7c931cf39d12
Peter van Meijgaard <peter@vivid-web.com>
2025-10-12 13:42:49 +0200

    feat: add Neon database provider

---
+++ packages/db/src/providers/neon/create-database.ts
@@ -0,0 +1,15 @@
+import { drizzle } from "drizzle-orm/neon-http";
+
+import type { createClient } from "./create-client.js";
+
+import * as schema from "../../schema.js";
+
+type Options = {
+	client: ReturnType<typeof createClient>;
+};
+
+export function createDatabase({ client }: Options) {
+	return drizzle({ schema, client, casing: "snake_case" });
+}
+
+export type Database = ReturnType<typeof createDatabase>;
